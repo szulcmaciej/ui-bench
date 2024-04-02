e60ffd2063ecebdd791ea3a2e37fdeeda6c447d9
Maciej Szulc <szulcmaciej1@gmail.com>
2024-04-02 20:54:17 +0100

    selenium docker firefox test

--- test_remote_firefox.py
+++ test_remote_firefox.py
@@ -1,5 +1,5 @@
 # docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-firefox:4.19.0-20240328
-# then open http://localhost:7900/?autoconnect=1&resize=scale&password=secret
+# for debugging or viewing open http://localhost:7900/?autoconnect=1&resize=scale&password=secret
 from selenium import webdriver
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
@@ -8,22 +8,17 @@ driver = webdriver.Remote(
    command_executor='http://127.0.0.1:4444/wd/hub',
    options=webdriver.FirefoxOptions()
 )
+try:
+   driver.get("https://www.windguru.cz/station/3428")
+   assert "Matas Blancas" in driver.title
+   driver.implicitly_wait(5)
+   import time
+   time.sleep(1)
+   elem = driver.find_element(By.CLASS_NAME, "wgs_wind_avg_value")
+   driver.save_screenshot('prtscrn.png')
+   wind_speed = elem.text
+   print(f'{wind_speed=}')
+   assert "No results found." not in driver.page_source
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-
-# move mouse
-# ActionChains(driver).move_by_offset(13, 15).perform()
-# driver.implicitly_wait(1)
-# ActionChains(driver).move_by_offset(13, 15).perform()
-# driver.implicitly_wait(1)
-# ActionChains(driver).move_by_offset(13, 15).perform()
-# driver.implicitly_wait(1)
-# ActionChains(driver).move_by_offset(13, 15).perform()
-# driver.close()
-driver.quit()
+finally:
+   driver.quit()
