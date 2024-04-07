dbf8401de560566d2c2d05fe3383067acc45f6f0
Maciej Szulc <szulcmaciej1@gmail.com>
2024-04-07 20:58:20 +0100

    run script - guest

--- run_guest.sh
+++ run_guest.sh
@@ -1,5 +1,11 @@
 # TODO (run this automatically after the VM is up)
 # clone the repo
+git clone https://github.com/szulcmaciej/ui-bench.git
+cd ui-bench
 # install dependencies
+uv venv venv
+source venv/bin/activate
+uv pip install -r requirements.txt
 # get API keys (how?)
 # run the program
+python3 test_move_mouse.py
--- test_move_mouse.py
+++ test_move_mouse.py
@@ -1,7 +1,7 @@
 import random
 import pyautogui
 
-for i in range(3):
+for i in range(50):
     random_x = random.randint(-100, 100)
     random_y = random.randint(-100, 100)
     pyautogui.moveRel(random_x, random_y, 0.2, pyautogui.easeInOutQuad)
