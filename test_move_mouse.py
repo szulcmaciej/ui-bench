8b173b455e0ea78550e4d77758cce676ce602114
Maciej Szulc <szulcmaciej1@gmail.com>
2024-04-07 18:26:35 +0100

    move mouse update

--- test_move_mouse.py
+++ test_move_mouse.py
@@ -1,9 +1,8 @@
 import random
-import time
 import pyautogui
 
 for i in range(3):
     random_x = random.randint(-100, 100)
     random_y = random.randint(-100, 100)
     pyautogui.moveRel(random_x, random_y, 0.2, pyautogui.easeInOutQuad)
-    time.sleep(0.1)
+    print(pyautogui.position())
