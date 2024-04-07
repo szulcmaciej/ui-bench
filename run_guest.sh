023d8c605e17fea6b212a297f737598377f6beba
Maciej Szulc <szulcmaciej1@gmail.com>
2024-04-07 22:10:21 +0100

    additional git pull on guest to streamline bug-fixing

--- run_guest.sh
+++ run_guest.sh
@@ -1,13 +1,17 @@
 # TODO (run this automatically after the VM is up)
 
 # clone the repo
-git clone https://github.com/szulcmaciej/ui-bench.git
+git clone https://github.com/szulcmaciej/ui-bench.git # if there is no repo yet
 cd ui-bench
+git pull # if there is a repo, pull the latest changes
+
 # install python dependencies
 uv venv venv
 source venv/bin/activate
 uv pip install -r requirements.txt
 
 # get API keys (how?)
+# TODO
+
 # run the program
 python3 main.py
