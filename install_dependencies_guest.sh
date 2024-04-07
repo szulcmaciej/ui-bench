36b66f038de39e61c4c329261421e3b5215756b7
Maciej Szulc <szulcmaciej1@gmail.com>
2024-04-07 22:05:05 +0100

    more guest dependencies

--- install_dependencies_guest.sh
+++ install_dependencies_guest.sh
@@ -1,3 +1,5 @@
 # as this needs to be run with root privileges, it should be already included in the VM image
-apt-get update && apt-get install -y git curl
-curl -LsSf https://astral.sh/uv/install.sh | sh
+apt-get update && apt-get install -y git curl python3-pip python3-tk python3-dev
+#curl -LsSf https://astral.sh/uv/install.sh | sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+# in virt-manager VM options, set video to Bochs or VGA for guest mouse to stay on the screen
